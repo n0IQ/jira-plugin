fea3dd04c8f3c749a0fe3996564eb797a8a7b4c3
n0IQ <varmamohit122@gmail.com>
2023-06-18 21:12:38 +0530

    initial set up using atlassian sdk

---
+++ src/main/java/com/atlassian/tutorial/myPlugin/impl/MyPluginComponentImpl.java
@@ -0,0 +1,25 @@
+package com.atlassian.tutorial.myPlugin.impl;
+
+import com.atlassian.sal.api.ApplicationProperties;
+import com.atlassian.tutorial.myPlugin.api.MyPluginComponent;
+
+
+public class MyPluginComponentImpl implements MyPluginComponent
+{
+        private final ApplicationProperties applicationProperties;
+
+        public MyPluginComponentImpl(final ApplicationProperties applicationProperties)
+    {
+        this.applicationProperties = applicationProperties;
+    }
+
+    public String getName()
+    {
+        if(null != applicationProperties)
+        {
+            return "myComponent:" + applicationProperties.getDisplayName();
+        }
+        
+        return "myComponent";
+    }
+}
