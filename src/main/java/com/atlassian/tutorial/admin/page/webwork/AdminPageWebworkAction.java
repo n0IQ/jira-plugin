2b6bc2fd433a76f191d73550abc80067739be417
n0IQ <varmamohit122@gmail.com>
2023-06-20 03:15:25 +0530

    Created new page using webwork module

---
+++ src/main/java/com/atlassian/tutorial/admin/page/webwork/AdminPageWebworkAction.java
@@ -0,0 +1,15 @@
+package com.atlassian.tutorial.admin.page.webwork;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.atlassian.jira.web.action.JiraWebActionSupport;
+
+public class AdminPageWebworkAction extends JiraWebActionSupport {
+    private static final Logger log = LoggerFactory.getLogger(AdminPageWebworkAction.class);
+
+    @Override
+    public String execute() throws Exception {
+        return "Admin Page";
+//                super.execute(); //returns SUCCESS
+    }
+}
---
+++ src/test/java/ut/com/atlassian/tutorial/admin/page/webwork/AdminPageWebworkActionTest.java
@@ -0,0 +1,34 @@
+package ut.com.atlassian.tutorial.admin.page.webwork;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.atlassian.tutorial.admin.page.webwork.AdminPageWebworkAction;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class AdminPageWebworkActionTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //AdminPageWebworkAction testClass = new AdminPageWebworkAction();
+
+        throw new Exception("AdminPageWebworkAction has no tests!");
+
+    }
+
+}
